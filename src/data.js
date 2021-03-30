079ce16135014da5b2afabdecbbc4a27a8006d21
rahmatagungj <rahmatagungj@gmail.com>
2021-03-31 03:03:31 +0700

    :sparkles: Adding new songs and fix layout

--- src/data.js
+++ src/data.js
@@ -22,12 +22,52 @@ function Pop() {
       active: false,
     },
     {
+      name: "At My Worst",
+      cover:
+        "https://lastfm.freetls.fastly.net/i/u/300x300/9b91876ca12cb8a2c138626af23f35e8.png",
+      artist: "Pink Sweet",
+      audio: "https://g.top4top.io/m_1915qrdla1.mp3",
+      color: ["#F0AADB", "#9A3442"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
       name: "LoveSick Girls",
       cover:
         "https://i.pinimg.com/originals/b7/8b/9b/b78b9b03ad8f13f3d409859f54a5e61f.png",
       artist: "BlackPink",
       audio: "https://k.top4top.io/m_1915vsdt93.mp3",
       color: ["#F0A4BD", "#2bf3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Cuek",
+      cover:
+        "http://mavemagz.com/wp-content/uploads/2020/08/RIZKY-FEBIAN-X-ANYA-GERALDINE-02-290x290.jpg",
+      artist: "Rizky Febian",
+      audio: "https://c.top4top.io/m_19156igj11.mp3",
+      color: ["#231854", "#E1961B"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Playing With Fire",
+      cover:
+        "https://i.pinimg.com/originals/73/37/fc/7337fc754bddc0e39963ac49ea286fe2.jpg",
+      artist: "BlackPink",
+      audio: "https://a.top4top.io/m_1915cvopj1.mp3",
+      color: ["#803E1A", "#D19B23"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Hope Not",
+      cover:
+        "https://i.pinimg.com/originals/a2/cb/e8/a2cbe8db91de0eeb3bc2095035db1023.jpg",
+      artist: "BlackPink",
+      audio: "https://i.top4top.io/m_1915lonhi1.mp3",
+      color: ["#C3402F", "#C23F20"],
       id: uuidv4(),
       active: false,
     },
