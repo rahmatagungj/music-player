f0ee995e6bb102f5a13a79caa970d19379d394b7
rahmatagungj <rahmatagungj@gmail.com>
2021-04-01 03:17:06 +0700

    :art: move to app subdomain

--- deploy.py
+++ deploy.py
@@ -2,10 +2,8 @@ import os
 import build
 
 print("\n== COPYING FILES TO PRODUCTION")
-print(
-    "Copying all files : build/ -> ../production-rahmatagungjulians.tech/app/music-player/"
-)
-os.system("cp -r build/* ../production-rahmatagungjulians.tech/app/music-player")
+print("Copying all files : build/ -> ../app-rahmatagungjulians.tech/music-player/")
+os.system("cp -r build/* ../app-rahmatagungjulians.tech/music-player")
 print("\n== REMOVING BUILD FOLDER")
 os.system("sudo rm -rf build")
 print("\n==== DEPLOY DONE ====")
